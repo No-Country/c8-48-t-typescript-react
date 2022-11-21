7066a7f7d61feef2b6a6edf72f090c648aa28ec6
David Enamorado <david8enaceituno@gmail.com>
2022-11-21 15:31:27 -0600

    add: uso de env del port backend y eliminacion de console log en servicios

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -54,7 +54,6 @@ export class AuthService {
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
     }
-    console.log(error);
     throw new InternalServerErrorException(
       'Error interno, por favor contacte al administrador',
     );
--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(Number(process.env.BACKEND_PORT));
 }
 bootstrap();

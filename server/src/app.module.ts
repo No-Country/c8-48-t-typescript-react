674572f9351bf61eebcef2bc427be9dbeb975c43
David Enamorado <david8enaceituno@gmail.com>
2022-11-21 15:39:13 -0600

    add: config env y config conexion DB Postgres

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_DB_HOST,
+      port: Number(process.env.POSTGRES_DB_PORT),
+      database: process.env.POSTGRES_DB_NAME,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'dev',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

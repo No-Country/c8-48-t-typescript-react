065551af499810a9724339f413976a717822d77c
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2022-11-21 14:52:01 -0300

    fixing route

--- client/src/routes/auth.routes.tsx
+++ client/src/routes/auth.routes.tsx
@@ -3,14 +3,10 @@ import { lazy } from 'react';
 
 const Landing = lazy(() => import('../views/error/Error404'));
 const DefaultLayout = lazy(() => import('../views/DefaultLayout'));
-const SignUpPageAthlete = lazy(() => import('../views/SignUpAthlete'));
-const SignUpPageUniversity = lazy(() => import('../views/SignUpUniversity'));
 
 export const routesAuth: JSX.Element = (
-  <Route path="/auth" element={<Landing />}>
+  <Route path="auth" element={<Landing />}>
     <Route path="login" element={<DefaultLayout />} />
-    <Route path="signup/athlete" element={<SignUpPageAthlete />} />
-    <Route path="signup/university" element={<SignUpPageUniversity />} />
     <Route path="error" element={<DefaultLayout />} />
   </Route>
 );

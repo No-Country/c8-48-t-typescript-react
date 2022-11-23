46c447b2f17be0391e3973e6657f32c969de0877
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2022-11-22 22:28:22 -0300

    Adding links to home

--- client/src/views/Home/index.tsx
+++ client/src/views/Home/index.tsx
@@ -1,3 +1,18 @@
-const Home = () => <div>Home view</div>;
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  const links = ['/login/universities', '/login/athletes'];
+  return (
+    <>
+      <Typography variant="h5">Home view</Typography>
+      {links.map((value) => (
+        <Link to={value}>
+          <Typography variant="h6">{value}</Typography>
+        </Link>
+      ))}
+    </>
+  );
+};
 
 export default Home;

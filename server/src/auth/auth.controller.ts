579e53e4f97d6b3a607a010c061eafc839c8e54c
David Enamorado <david8enaceituno@gmail.com>
2022-11-24 13:41:30 -0600

    add: mandar el jwt endpoint login

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -13,6 +13,7 @@ export class AuthController {
   }
 
   @Post('login')
+  @HttpCode(200)
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }

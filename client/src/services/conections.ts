44a618437c90eeaa333046c203b5de5dbf19c2f9
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2022-11-30 17:05:05 -0300

    more things

--- client/src/services/conections.ts
+++ client/src/services/conections.ts
@@ -1,3 +1,7 @@
+import { rootBackEnd } from '../constants/links';
+
+const completeUrl = (url = '') => rootBackEnd + url;
+
 const beaverHeader: () => HeadersInit = () => {
   return { 'Content-Type': 'application/json' };
 };
@@ -6,17 +10,14 @@ type postLogin = {
   email: string;
   password: string;
 };
-/**  email: string;
-  password: string; */
 const postLogin = async (body: postLogin) => {
   try {
-    const _data = await fetch('http://localhost:3000/api/auth/login', {
+    const _data = await fetch(completeUrl('api/auth/login'), {
       method: 'POST',
       headers: beaverHeader(),
       body: JSON.stringify(body),
     });
     const data = await _data.json();
-    console.log('file: conections.ts:6  postLogin  data', data);
   } catch (e) {
     console.log(e);
   }

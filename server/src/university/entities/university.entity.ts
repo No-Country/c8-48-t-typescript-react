b821f321904df2f5720780e622e7ef9b5ce5e9dd
David Enamorado <david8enaceituno@gmail.com>
2022-11-30 02:04:45 -0600

    add: creacion del entity del modulo university

--- server/src/university/entities/university.entity.ts
+++ server/src/university/entities/university.entity.ts
@@ -1,1 +1,24 @@
-export class University {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Universities')
+export class University {
+  @PrimaryGeneratedColumn('uuid')
+  idUniversity: string;
+
+  @Column('int')
+  idCountry: number;
+
+  @Column({ type: 'text', length: 255 })
+  linkedin: string;
+
+  @Column({ type: 'text', length: 255 })
+  website: string;
+
+  @Column({ type: 'text', length: 255 })
+  description: string;
+
+  @Column('bool')
+  acceptConditions: boolean;
+
+  //TODO: Relación con user
+}
--- server/src/university/university.module.ts
+++ server/src/university/university.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UniversityService } from './university.service';
 import { UniversityController } from './university.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { University } from './entities/university.entity';
 
 @Module({
   controllers: [UniversityController],
-  providers: [UniversityService]
+  providers: [UniversityService],
+  imports: [TypeOrmModule.forFeature([University])],
+  exports: [TypeOrmModule],
 })
 export class UniversityModule {}

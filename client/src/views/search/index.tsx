0f1b68cb88571abdefaa140e96a97c34e43d6384
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2022-12-08 14:35:52 -0300

    Tarjetas de universidad despues de las de persona

--- client/src/views/search/index.tsx
+++ client/src/views/search/index.tsx
@@ -10,8 +10,8 @@ const SearchView = () => {
   const [searchParams] = useSearchParams();
   const { search } = useParams();
   const { searchAthlete, searchUniversity } = useRequestAuth();
-  const [athletes, setAthletes] = useState([]);
-  // const [universities, setUniversities] = useState([]);
+  const [athletes, setAthletes] = useState(null);
+  const [universities, setUniversities] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
     if (!localStorage.getItem('token')) {
@@ -34,19 +34,53 @@ const SearchView = () => {
         })
         .catch(console.error);
     };
+    const getUniversitySearch = async (search = '') => {
+      await fetch(
+        'https://nc-backend-production.up.railway.app/api/university/search/' +
+          search,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        },
+      )
+        .then((res) => {
+          const json = res.json();
+          return json;
+        })
+        .then((res) => {
+          setUniversities(res);
+        })
+        .catch(console.error);
+    };
 
     if (!athletes && search) getAthleteSearch(search);
-    // searchUniversity(search || '')?.then((res) => {
-    //   setUniversities(res || []);
-    // });
-  }, [search, searchAthlete, searchUniversity, athletes]);
+    if (!universities && search) getUniversitySearch(search);
+  }, [search, searchAthlete, searchUniversity, athletes, universities]);
 
+  console.log(athletes);
+  console.log(universities);
   return (
     <MainContainer>
       <PrincipalContainer>
         {!!athletes &&
-          athletes.map((athlete: AthleteData) => athlete && <CardFilter variation="athlete" />)}
-        {/* {universities && universities?.map(() => <CardFilter variation="university" />)} */}
+          athletes.map((e) => {
+            return (
+              <CardFilter
+                variation="athlete"
+                id={e.idUser}
+                fullName={e.fullName}
+              />
+            );
+          })}
+        {!!universities &&
+          universities.map((e) => {
+            return (
+              <CardFilter
+                variation="university"
+                id={e.idUser}
+                fullName={e.fullName}
+              />
+            );
+          })}
       </PrincipalContainer>
     </MainContainer>
   );
